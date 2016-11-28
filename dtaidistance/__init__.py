9396d35338efb4e82e5a00e22600a4e17b9d2ee2
Wannes Meert <wannes.meert@cs.kuleuven.be>
2016-11-28 17:34:51 +0100

    Improve init file

--- dtaidistance/__init__.py
+++ dtaidistance/__init__.py
@@ -1,3 +1,8 @@
-from . import dtw, dtw_c
+from . import dtw
+try:
+    from . import dtw_c
+except ImportError:
+    print("DTW C variant not available, run `make build` in the dtaidistance directory")
+    dtw_c = None
 
 __version__ = "0.1.0"
