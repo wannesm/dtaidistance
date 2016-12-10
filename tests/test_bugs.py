f9e1ea537a7d139a6941dca8e564bbd2b7dc3ad4
Wannes Meert <wannes.meert@cs.kuleuven.be>
2016-12-10 11:10:24 +0100

    Update tests

--- tests/test_bugs.py
+++ tests/test_bugs.py
@@ -75,7 +75,6 @@ def test_distance2_c():
     s2 = np.array([0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0])
     d1 = dtw.distance(s1, s2, **dist_opts)
     d2 = dtw_c.distance_nogil(s1, s2, **dist_opts)
-    print(d1, d2)
     assert d1 == d2
     assert d1 == pytest.approx(1.0)
 
@@ -84,8 +83,10 @@ def test_distance3_a():
     dist_opts = {"penalty": 0.005, "max_step": 0.011, "window": 3}
     s = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.005, 0.01, 0.015, 0.02, 0.01, 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
     p = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.005, 0.01, 0.015, 0.02, 0.01, 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
-    d = dtw_c.distance_nogil(s, p, **dist_opts)
-    print(d)
+    d1 = dtw.distance(s, p, **dist_opts)
+    d2 = dtw_c.distance_nogil(s, p, **dist_opts)
+    assert d1 == pytest.approx(d2)
+
 
 # test_distance2_a()
 # test_distance2_b()
