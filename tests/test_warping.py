234079652269995e2e7d05de532a0dd267dfd0e0
Wannes Meert <wannes.meert@cs.kuleuven.be>
2017-12-11 15:56:55 +0100

    Fix tests

--- tests/test_warping.py
+++ tests/test_warping.py
@@ -8,8 +8,8 @@ def test_normalize():
     s1 = np.array([0., 0, 1, 2, 1, 0, 1, 0, 0, 2, 1, 0, 0])
     s2 = np.array([0., 1, 2, 3, 1, 0, 0, 0, 2, 1, 0, 0, 0])
     r = dtw.warp(s1, s2, plot=False)
-    r_c = np.array([0., 1., 2., 2., 1., 0.25, 0., 0., 2., 1., 0., 0., 0.])
-    dtw.plot_warping(s1, s2)
+    r_c = np.array([0., 1., 2., 2., 1., 0.5, 0., 0., 2., 1., 0., 0., 0.])
+    # dtw.plot_warping(s1, s2)
     np.testing.assert_almost_equal(r, r_c, decimal=4)
 
 
@@ -18,10 +18,10 @@ def test_psi_dtw():
     s1 = np.sin(x)
     s2 = np.sin(x - 1)
 
-    dtw.plot_warping(s1, s2, psi=20)
+    # dtw.plot_warping(s1, s2, psi=20)
 
 
 if __name__ == "__main__":
     np.set_printoptions(precision=2, linewidth=120)
-    # test_normalize()
-    test_psi_dtw()
+    test_normalize()
+    # test_psi_dtw()
