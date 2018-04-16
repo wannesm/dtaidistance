dc99970657222246286dbad3817c2be65cb6a0f4
Wannes Meert <wannes.meert@cs.kuleuven.be>
2018-04-16 23:49:11 +0200

    Fix tests

--- tests/test_dtw2d.py
+++ tests/test_dtw2d.py
@@ -1,15 +1,15 @@
 import math
 import pytest
 import numpy as np
-from dtaidistance import dtw2d
+from dtaidistance import dtwndim
 
 
 def test_distance1_a():
     s1 = np.array([[0, 0], [0, 1], [2, 1], [0, 1],  [0, 0]], dtype=np.double)
     s2 = np.array([[0, 0], [2, 1], [0, 1], [0, .5], [0, 0]], dtype=np.double)
-    d1 = dtw2d.distance(s1, s2)
+    d1 = dtwndim.distance(s1, s2)
     print(d1)
-    d1p, paths = dtw2d.warping_paths(s1, s2)
+    d1p, paths = dtwndim.warping_paths(s1, s2)
     print(d1p)
     print(paths)
     assert d1 == pytest.approx(d1p)
