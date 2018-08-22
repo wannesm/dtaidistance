4852ce64aff97dc233032b4594c03ff8f4d0c347
Wannes Meert <wannes.meert@cs.kuleuven.be>
2018-08-22 12:22:53 +0200

    Cleanup benchmark

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -4,18 +4,21 @@ import array
 import pytest
 import math
 
-n=10
+n = 1
+nn = 100
 
 
-## DISTANCE 1 ##
+# --- DISTANCE 1 ---
 
 
 @pytest.mark.benchmark(group="distance1")
 def test_distance1_python_compress(benchmark):
     s1 = [0, 0, 1, 2, 1, 0, 1, 0, 0]*n
     s2 = [0, 1, 2, 0, 0, 0, 0, 0, 0]*n
+
     def d():
         return dtw.distance(s1, s2)
+
     assert benchmark(d) == math.sqrt(2)*n
 
 
@@ -23,9 +26,11 @@ def test_distance1_python_compress(benchmark):
 def test_distance1_python_matrix(benchmark):
     s1 = [0, 0, 1, 2, 1, 0, 1, 0, 0]*n
     s2 = [0, 1, 2, 0, 0, 0, 0, 0, 0]*n
+
     def d():
         dd, _ = dtw.warping_paths(s1, s2)
         return dd
+
     assert benchmark(d) == math.sqrt(2)*n
 
 
@@ -33,8 +38,10 @@ def test_distance1_python_matrix(benchmark):
 def test_distance1_cpython(benchmark):
     s1 = np.array([0., 0, 1, 2, 1, 0, 1, 0, 0]*n)
     s2 = np.array([0., 1, 2, 0, 0, 0, 0, 0, 0]*n)
+
     def d():
         return dtw_c.distance(s1, s2)
+
     assert benchmark(d) == math.sqrt(2)*n
 
 
@@ -42,24 +49,26 @@ def test_distance1_cpython(benchmark):
 def test_distance1_c_array(benchmark):
     s1 = np.array([0., 0, 1, 2, 1, 0, 1, 0, 0]*n)
     s2 = np.array([0., 1, 2, 0, 0, 0, 0, 0, 0]*n)
+
     def d():
         return dtw_c.distance_nogil(s1, s2)
+
     assert benchmark(d) == math.sqrt(2)*n
 
 
 @pytest.mark.benchmark(group="distance1")
 def test_distance1_c_numpy(benchmark):
-    s1 = array.array('d',[0., 0, 1, 2, 1, 0, 1, 0, 0]*n)
-    s2 = array.array('d',[0., 1, 2, 0, 0, 0, 0, 0, 0]*n)
+    s1 = array.array('d', [0., 0, 1, 2, 1, 0, 1, 0, 0]*n)
+    s2 = array.array('d', [0., 1, 2, 0, 0, 0, 0, 0, 0]*n)
+
     def d():
         return dtw_c.distance_nogil(s1, s2)
+
     assert benchmark(d) == math.sqrt(2)*n
 
 
-## DISTANCE MATRIX 1 ##
+# --- DISTANCE MATRIX 1 ---
 
-n = 1
-nn = 100
 
 @pytest.mark.benchmark(group="matrix1")
 def test_distance_matrix1_serialpython(benchmark):
@@ -67,10 +76,12 @@ def test_distance_matrix1_serialpython(benchmark):
          [0, 1, 2, 0, 0, 0, 0, 0, 0] * n,
          [1, 2, 0, 0, 0, 0, 0, 1] * n] * nn
     s = [np.array(si) for si in s]
+
     def d():
         return dtw.distance_matrix(s, parallel=False, use_c=False, use_nogil=False)
+
     m = benchmark(d)
-    assert m[0,1] == math.sqrt(2)*n
+    assert m[0, 1] == math.sqrt(2)*n
 
 
 @pytest.mark.benchmark(group="matrix1")
@@ -79,10 +90,12 @@ def test_distance_matrix1_parallelpython(benchmark):
          [0, 1, 2, 0, 0, 0, 0, 0, 0] * n,
          [1, 2, 0, 0, 0, 0, 0, 1] * n] * nn
     s = [np.array(si) for si in s]
+
     def d():
         return dtw.distance_matrix(s, parallel=True, use_c=False, use_nogil=False)
+
     m = benchmark(d)
-    assert m[0,1] == math.sqrt(2)*n
+    assert m[0, 1] == math.sqrt(2)*n
 
 
 @pytest.mark.benchmark(group="matrix1")
@@ -91,10 +104,12 @@ def test_distance_matrix1_serialpythonc(benchmark):
          [0., 1, 2, 0, 0, 0, 0, 0, 0] * n,
          [1., 2, 0, 0, 0, 0, 0, 1] * n] * nn
     s = [np.array(si) for si in s]
+
     def d():
         return dtw.distance_matrix(s, parallel=False, use_c=True, use_nogil=False)
+
     m = benchmark(d)
-    assert m[0,1] == math.sqrt(2)*n
+    assert m[0, 1] == math.sqrt(2)*n
 
 
 @pytest.mark.benchmark(group="matrix1")
@@ -103,10 +118,12 @@ def test_distance_matrix1_parallelpythonc(benchmark):
          [0., 1, 2, 0, 0, 0, 0, 0, 0] * n,
          [1., 2, 0, 0, 0, 0, 0, 1] * n] * nn
     s = [np.array(si) for si in s]
+
     def d():
         return dtw.distance_matrix(s, parallel=True, use_c=True, use_nogil=False)
+
     m = benchmark(d)
-    assert m[0,1] == math.sqrt(2)*n
+    assert m[0, 1] == math.sqrt(2)*n
 
 
 @pytest.mark.benchmark(group="matrix1")
@@ -115,10 +132,12 @@ def test_distance_matrix1_serialpurec(benchmark):
          [0., 1, 2, 0, 0, 0, 0, 0, 0]*n,
          [1., 2, 0, 0, 0, 0, 0, 1]*n]*nn
     s = [np.array(si) for si in s]
+
     def d():
         return dtw.distance_matrix(s, parallel=False, use_c=True, use_nogil=True)
+
     m = benchmark(d)
-    assert m[0,1] == math.sqrt(2)*n
+    assert m[0, 1] == math.sqrt(2)*n
 
 
 @pytest.mark.benchmark(group="matrix1")
@@ -127,17 +146,15 @@ def test_distance_matrix1_parallelpurec(benchmark):
          [0., 1, 2, 0, 0, 0, 0, 0, 0] * n,
          [1., 2, 0, 0, 0, 0, 0, 1] * n] * nn
     s = [np.array(si) for si in s]
+
     def d():
         return dtw.distance_matrix(s, parallel=True, use_c=True, use_nogil=True)
+
     m = benchmark(d)
-    assert m[0,1] == math.sqrt(2)*n
+    assert m[0, 1] == math.sqrt(2)*n
 
 
-## CLUSTER MATRIX 1 ##
-
-n = 1
-nn = 100
-
+# --- CLUSTER MATRIX 1 ---
 
 @pytest.mark.benchmark(group="cluster1")
 def test_cluster1_hierarchical(benchmark):
@@ -145,10 +162,12 @@ def test_cluster1_hierarchical(benchmark):
          [0, 1, 2, 0, 0, 0, 0, 0, 0] * n,
          [1, 2, 0, 0, 0, 0, 0, 1] * n] * nn
     s = [np.array(si) for si in s]
+
     def d():
         c = clustering.Hierarchical(dtw.distance_matrix_fast, {})
         return c.fit(s)
-    m = benchmark(d)
+
+    benchmark(d)
 
 
 @pytest.mark.benchmark(group="cluster1")
@@ -157,10 +176,12 @@ def test_cluster1_linkage(benchmark):
          [0, 1, 2, 0, 0, 0, 0, 0, 0] * n,
          [1, 2, 0, 0, 0, 0, 0, 1] * n] * nn
     s = [np.array(si) for si in s]
+
     def d():
         c = clustering.LinkageTree(dtw.distance_matrix_fast, {})
         return c.fit(s)
-    m = benchmark(d)
+
+    benchmark(d)
 
 
 if __name__ == "__main__":
