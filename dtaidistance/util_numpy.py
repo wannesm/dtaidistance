10643ab41c4851ea00f3a209166cb78c7ad3196a
Wannes Meert <wannes.meert@cs.kuleuven.be>
2021-01-02 15:48:19 +0100

    improve tests

--- dtaidistance/util_numpy.py
+++ dtaidistance/util_numpy.py
@@ -2,7 +2,7 @@ from contextlib import ContextDecorator
 import os
 import logging
 
-from .exceptions import NumpyException
+from .exceptions import NumpyException, ScipyException
 
 
 logger = logging.getLogger("be.kuleuven.dtai.distance")
@@ -12,6 +12,12 @@ try:
     import numpy as np
 except ImportError:
     np = None
+
+
+try:
+    import scipy
+except ImportError:
+    scipy = None
 
 
 def test_without_numpy():
@@ -49,6 +55,21 @@ class NumpyStub:
         return getattr(np, name)
 
 
+class ScipyStub:
+    def __init__(self, testwithoutscipy):
+        self.testwithoutscipy = testwithoutscipy
+
+    def __getattr__(self, name):
+        if self.testwithoutscipy or scipy is None:
+            raise ScipyException("Scipy excepted to be available for test. "
+                                 "Set DTAIDISTANCE_TESTWITHOUTSCIPY=1 to test without Scipy.")
+        return getattr(scipy, name)
+
+    def import_signal(self):
+        from scipy import signal
+        return signal
+
+
 class test_uses_numpy(ContextDecorator):
     def __init__(self, strict=True):
         """Context to construct tests that use the optional dependency Numpy.
@@ -72,4 +93,30 @@ class test_uses_numpy(ContextDecorator):
                 else:
                     return
             if issubclass(exc[0], NumpyException):
+                return True
+
+
+class test_uses_scipy(ContextDecorator):
+    def __init__(self, strict=True):
+        """Context to construct tests that use the optional dependency Scipy.
+
+        :param strict: Throw error if Scipy is not used (to remove context where not necessary)
+        :return: Numpy stub
+        """
+        self.strict = strict
+        self.testwithoutscipy = test_without_scipy()
+
+    def __enter__(self):
+        return ScipyStub(self.testwithoutscipy)
+
+    def __exit__(self, *exc):
+        if self.testwithoutscipy:
+            if exc[0] is None:
+                if self.strict and self.testwithoutscipy:
+                    # If no ScipyException is thrown, this test did not use Scipy because no error was thrown
+                    # and should not use this decorator
+                    raise Exception("Test does not use Scipy, remove decorator!")
+                else:
+                    return
+            if issubclass(exc[0], ScipyException):
                 return True
