921b6fd50f03f225f4b8cc9eeb07db5180efb8b2
wannesm <wannesm@gmail.com>
2022-10-22 14:08:22 +0200

    Make it easier to use float/int

--- dtaidistance/jinja/generate.py
+++ dtaidistance/jinja/generate.py
@@ -24,34 +24,37 @@ seq_tpy = "double"
 seq_format = "d"  # https://docs.python.org/3/library/array.html
 # Also change the type in lib/DTAIDistanceC/DTAIDistanceC/dd_globals.h
 
-set_vars = {"seq_tpy": seq_tpy, "seq_t": seq_t, "seq_format": seq_format}
+
+def set_vars():
+    return {"seq_tpy": seq_tpy, "seq_t": seq_t, "seq_format": seq_format}
 
 
-targets = {
-    "dtw_cc.pyx":
-        ["dtw_cc.jinja.pyx",
-            set_vars,
-            ["dtw_cc_warpingpaths.jinja.pyx",
-             "dtw_cc_distancematrix.jinja.pyx",
-             "dtw_cc_warpingpath.jinja.pyx",
-             "dtw_cc_dba.jinja.pyx"]],
-    "dtw_cc_omp.pyx":
-        ["dtw_cc_omp.jinja.pyx",
-            set_vars,
-            []],
-    "dtw_cc.pxd":
-        ["dtw_cc.jinja.pxd",
-            set_vars,
-            []],
-    "dtaidistancec_globals.pxd":
-        ["dtaidistancec_globals.jinja.pxd",
-            set_vars,
-            []],
-    "ed_cc.pyx":
-        ["ed_cc.jinja.pyx",
-            set_vars,
-            []],
-}
+def targets():
+    return {
+        "dtw_cc.pyx":
+            ["dtw_cc.jinja.pyx",
+                set_vars(),
+                ["dtw_cc_warpingpaths.jinja.pyx",
+                 "dtw_cc_distancematrix.jinja.pyx",
+                 "dtw_cc_warpingpath.jinja.pyx",
+                 "dtw_cc_dba.jinja.pyx"]],
+        "dtw_cc_omp.pyx":
+            ["dtw_cc_omp.jinja.pyx",
+                set_vars(),
+                []],
+        "dtw_cc.pxd":
+            ["dtw_cc.jinja.pxd",
+                set_vars(),
+                []],
+        "dtaidistancec_globals.pxd":
+            ["dtaidistancec_globals.jinja.pxd",
+                set_vars(),
+                []],
+        "ed_cc.pyx":
+            ["ed_cc.jinja.pyx",
+                set_vars(),
+                []],
+    }
 essential_targets = ['dtw_cc.pyx', 'dtw_cc.pxd', 'dtaidistancec_globals.pxd',
                      'dtw_cc_omp.pyx', 'ed_cc.pyx']
 
@@ -59,7 +62,7 @@ essential_targets = ['dtw_cc.pyx', 'dtw_cc.pxd', 'dtaidistancec_globals.pxd',
 def generate(target):
     logger.info(f'Generating: {target}')
     fno = target
-    fni, kwargs, _deps = targets[target]
+    fni, kwargs, _deps = targets()[target]
     template = templateEnv.get_template(fni)
     outputText = template.render(**kwargs)
     with open(fno, 'w') as o:
@@ -68,17 +71,22 @@ def generate(target):
 
 def dependencies(target):
     logger.info(f'Dependencies for: {target}')
-    fni, _kwargs, deps = targets[target]
+    fni, _kwargs, deps = targets()[target]
     return [fni] + deps
 
 
 def main(argv=None):
+    global seq_t
+    global seq_tpy
+    global seq_format
+    global set_vars
+
     parser = argparse.ArgumentParser(description='Generate source code files from templates')
     parser.add_argument('--verbose', '-v', action='count', default=0, help='Verbose output')
     parser.add_argument('--quiet', '-q', action='count', default=0, help='Quiet output')
     parser.add_argument('--deps', '-d', action='store_true', help='Print dependencies')
     parser.add_argument('--targets', '-t', action='store_true', help='Print available targets')
-    # parser.add_argument('--output', '-o', required=True, help='Output file')
+    parser.add_argument('--seqt', help='Data type to use for values in the sequence (default double)')
     # parser.add_argument('--version', action='version', version='%(prog)s 1.0')
     parser.add_argument('input', nargs='*', help='List of target files to generate')
     args = parser.parse_args(argv)
@@ -88,15 +96,30 @@ def main(argv=None):
 
     if args.targets:
         print('Targets:')
-        for k in targets.keys():
+        for k in targets().keys():
             e = ' (default)' if k in essential_targets else ''
             print(f'- {k}{e}')
         return 0
 
+    if args.seqt:
+        if args.seqt == "double":
+            pass
+        elif args.seqt == "float":
+            seq_t = "float"
+            seq_tpy = "float"
+            seq_format = "f"
+        elif args.seqt == "int":
+            seq_t = "int"
+            seq_tpy = "int"
+            seq_format = "i"
+        else:
+            raise TypeError("seqt should be one of double/float/int")
+
+
     if args.input is None or len(args.input) == 0:
         inputs = essential_targets
     elif args.input[0] == "all":
-        inputs = targets.keys()
+        inputs = targets().keys()
     else:
         inputs = args.input
 
