e010034907cfb32bf4d2adfa50b007089a1186f5
wannesm <wannesm@gmail.com>
2022-10-25 14:18:04 +0200

    similarity

--- dtaidistance/similarity.py
+++ dtaidistance/similarity.py
@@ -9,7 +9,7 @@ def distance_to_similarity(D, r=None, method='exponential'):
 
     The avaiable methods are:
     - Exponential: e^(-D / r)
-      r is 1 if not given
+      r is max(D) if not given
     - Reciprocal: 1 / (r + D)
       r is 0.0000001 if not given
     - Reverse: r - D
@@ -23,8 +23,12 @@ def distance_to_similarity(D, r=None, method='exponential'):
     method = method.lower()
     if method == 'exponential':
         if r is None:
-            r = 1
+            r = np.max(D)
         S = np.exp(-D / r)
+    elif method == 'gaussian':
+        if r is None:
+            r = np.max(D)
+        S = np.exp(-np.power(D, 2) / r)
     elif method == 'reciprocal':
         if r is None:
             r = 0.0000001
@@ -33,4 +37,6 @@ def distance_to_similarity(D, r=None, method='exponential'):
         if r is None:
             r = np.min(D) + np.max(D)
         S = r - D
+    else:
+        raise ValueError("method={} is not supported".format(method))
     return S
